0fad669ac611d3e8be9b7c4dd153099c9f12e142
Jeannine257 <jeanninethiombiano502@gmail.com>
2026-02-23 01:41:22 +0000

    fix: remove invalid data prop from SalesChart

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -185,7 +185,7 @@ export default function AdminDashboard() {
       {/* Charts & Activity */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="lg:col-span-2">
-          <SalesChart data={chartData} />
+          <SalesChart />
         </div>
         <div>
           <ActivityTimeline activities={activities} />
